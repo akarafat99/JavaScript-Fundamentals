59742e4b528fedba314f8cd0d68fd84796676199
Abdul Khaled Arafat <arafatabdulkhaled@gmail.com>
2025-11-04 04:07:07 +0600

    study

---
+++ 01 js basic/js-basic.js
@@ -0,0 +1,4 @@
+// Open browser and goto inspect then goto console tab
+// Now type alert('hello World!'); then a popup will show the “Hello World” text
+// Type 2+3 and enter. Then you will see 5 as the ans
+// document.body.innerHTML = "Hello"; will show Hello in body section
